084b5cc54b5f91bd86c02ca1bd196f5ec9f839ae
minhphanhvu <minhphanh.vu@gmail.com>
2021-06-24 18:17:37 -0500

    Configure webpack using stylesheet_pack_tag

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   resources :posts, except: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :categories
+  resources :categories, only: [:index, :new, :show]
 end
