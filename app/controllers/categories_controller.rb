c148a4cb188f209fa47e2f10bb3696eb44208f92
minhphanhvu <minhphanh.vu@gmail.com>
2021-06-24 20:00:25 -0500

    Create new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,26 @@ class CategoriesController < ApplicationController
     @category = Category.find_by_id(params[:id])
     @category_posts = @category.posts
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.name = @category.name.downcase
+
+    if @category.save
+      flash[:notice] = 'A new category was created.'
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
