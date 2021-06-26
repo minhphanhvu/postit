990fe08dbc81e5d56c8932d3a59804549d53f2cf
minhphanhvu <minhphanh.vu@gmail.com>
2021-06-26 05:25:51 -0500

    Add helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fix_url(url)
+    url.start_with?('http://', 'https://') ? url : "http://#{url}"
+  end
+
+  def display_datetime(dt)
+    dt.strftime("%m/%d/%Y %l:%M%P %Z")
+  end
 end
