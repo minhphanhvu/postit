510f2a551a6fd9d3a96defe93676fc68262c0557
minhphanhvu <minhphanh.vu@gmail.com>
2021-07-04 20:08:10 -0500

    Require user before voting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def require_user
     if !logged_in?
       flash[:error] = 'You must be logged in to do that.'
-      redirect_to root_path
+      redirect_back fallback_location: root_path
     end
   end
 end
