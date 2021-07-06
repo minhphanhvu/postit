5865b2447d2f3aadebc6642fc66f0b5420bfcf46
minhphanhvu <minhphanh.vu@gmail.com>
2021-07-06 13:39:57 -0500

    AJAX for comment voting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,12 +17,18 @@ class CommentsController < ApplicationController
   def vote
     @comment = Comment.find_by_id(params[:id])
     existing_vote = @comment.votes.where(user_id: current_user.id)
-    if existing_vote.empty?
-      Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
-    else
-      existing_vote.first.update(vote: params[:vote])
+    respond_to do |format|
+      if existing_vote.empty?
+        Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
+      else
+        existing_vote.first.update(vote: params[:vote])
+      end
+      @upvote = params[:vote]
+      format.html do
+        redirect_back fallback_location: root_path
+      end
+      format.js
     end
-    redirect_back fallback_location: root_path
   end
 
   private
